d506aa03559301389e7832c1fd5105321a418df1
jinchao.xu <jinchao.xu3@renren-inc.com>
2014-12-11 17:16:03 +0800

    no change

--- src/main/java/com/meki/play/manager/CookieManager.java
+++ src/main/java/com/meki/play/manager/CookieManager.java
@@ -12,82 +12,88 @@ import javax.servlet.http.HttpServletResponse;
 
 
 public class CookieManager {
-	private static CookieManager instance;
+    private static CookieManager instance;
 
-	public static CookieManager getInstance() {
-		if (instance == null)
-			synchronized (CookieManager.class) {
-				if (instance == null)
-					instance = new CookieManager();
-			}
+    public static CookieManager getInstance() {
+        if (instance == null)
+            synchronized (CookieManager.class) {
+                if (instance == null)
+                    instance = new CookieManager();
+            }
 
-		return instance;
-	}
-	public String getCookie(HttpServletRequest request, String key) {
-		Cookie[] cookies = request.getCookies();
-		if (cookies == null || cookies.length == 0)
-			return null;
-		for (int i = 0; i < cookies.length; i++) {
-			if (cookies[i].getName().equals(key))
-				return cookies[i].getValue();
-			
-		}
-		return null;
-	}
+        return instance;
+    }
 
-	public void saveCookie(HttpServletResponse response, String key,
-			String value) {
-		this.saveCookie(response, key, value, -1,"/") ;
-	}
-	public void saveCookie(HttpServletResponse response, String key,
-			String value,String path) {
-		this.saveCookie(response, key, value, -1,path) ;
-	}
-	public void saveCookie(HttpServletResponse response, String key,
-			String value,int second,String path) {
-		Cookie cookie = new Cookie(key, value);
-		cookie.setPath(path);
-		cookie.setMaxAge(second);
-		cookie.setDomain("."+ cd ..OpiConstants.domainMain);
-		response.addCookie(cookie);
-	}
-	public void saveCookie(HttpServletResponse response, String key,
-			String value,int second,String path, String domain) {
-		Cookie cookie = new Cookie(key, value);
-		cookie.setPath(path);
-		cookie.setMaxAge(second);
-		cookie.setDomain(domain);
-		response.addCookie(cookie);
-	}	
-	public void clearCookie(HttpServletResponse response, String key,
-			int second,String path){
-		if(key.equals("xng")){
-			try{
-				throw new Exception() ;
-			}catch(Exception e){
-				e.printStackTrace(System.out) ;
-			}
-		}
-		Cookie cookie = new Cookie(key,null);
-		cookie.setPath(path);
-		cookie.setMaxAge(second);
-		cookie.setDomain("."+OpiConstants.domainMain);
-		response.addCookie(cookie) ;
-	}
-	public void clearCookie(HttpServletResponse response, String key,
-			int second,String path, String domain){
-		if(key.equals("xng")){
-			try{
-				throw new Exception() ;
-			}catch(Exception e){
-				e.printStackTrace(System.out) ;
-			}
-		}
-		Cookie cookie = new Cookie(key,null);
-		cookie.setPath(path);
-		cookie.setMaxAge(second);
-		cookie.setDomain(domain);
-		response.addCookie(cookie) ;
-	}	
+    public String getCookie(HttpServletRequest request, String key) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null || cookies.length == 0)
+            return null;
+        for (int i = 0; i < cookies.length; i++) {
+            if (cookies[i].getName().equals(key))
+                return cookies[i].getValue();
+
+        }
+        return null;
+    }
+
+    public void saveCookie(HttpServletResponse response, String key,
+                           String value) {
+        this.saveCookie(response, key, value, -1, "/");
+    }
+
+    public void saveCookie(HttpServletResponse response, String key,
+                           String value, String path) {
+        this.saveCookie(response, key, value, -1, path);
+    }
+
+    public void saveCookie(HttpServletResponse response, String key,
+                           String value, int second, String path) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setPath(path);
+        cookie.setMaxAge(second);
+        cookie.setDomain("." + OpiConstants.domainMain);
+        response.addCookie(cookie);
+    }
+
+    public void saveCookie(HttpServletResponse response, String key,
+                           String value, int second, String path, String domain) {
+        Cookie cookie = new Cookie(key, value);
+        cookie.setPath(path);
+        cookie.setMaxAge(second);
+        cookie.setDomain(domain);
+        response.addCookie(cookie);
+    }
+
+    public void clearCookie(HttpServletResponse response, String key,
+                            int second, String path) {
+        if (key.equals("xng")) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        Cookie cookie = new Cookie(key, null);
+        cookie.setPath(path);
+        cookie.setMaxAge(second);
+        cookie.setDomain("." + OpiConstants.domainMain);
+        response.addCookie(cookie);
+    }
+
+    public void clearCookie(HttpServletResponse response, String key,
+                            int second, String path, String domain) {
+        if (key.equals("xng")) {
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
+        }
+        Cookie cookie = new Cookie(key, null);
+        cookie.setPath(path);
+        cookie.setMaxAge(second);
+        cookie.setDomain(domain);
+        response.addCookie(cookie);
+    }
 
 }
